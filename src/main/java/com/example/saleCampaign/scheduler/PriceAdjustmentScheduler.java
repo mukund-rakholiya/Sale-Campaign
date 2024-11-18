accc06a97181a3f47a06055f0ca546bca0586aa2
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-18 10:05:53 +0530

    Update PriceAdjustmentScheduler.java

--- src/main/java/com/example/saleCampaign/scheduler/PriceAdjustmentScheduler.java
+++ src/main/java/com/example/saleCampaign/scheduler/PriceAdjustmentScheduler.java
@@ -4,7 +4,7 @@ import com.example.saleCampaign.Model.Campaign;
 import com.example.saleCampaign.Model.CampaignDiscount;
 import com.example.saleCampaign.Model.PriceHistory;
 import com.example.saleCampaign.Model.Product;
-import com.example.saleCampaign.Repository.CampaignRepository;
+import com.example.saleCampaign.Repository.CampaignRepository; 
 import com.example.saleCampaign.Repository.PriceHistoryRepository;
 import com.example.saleCampaign.Repository.ProductRepository;
 import com.example.saleCampaign.Service.ProductService;
