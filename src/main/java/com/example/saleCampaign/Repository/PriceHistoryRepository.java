74b6bd024f339975e7e05d6b67e7bb07ae986182
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-18 10:04:13 +0530

    Update PriceHistoryRepository.java

--- src/main/java/com/example/saleCampaign/Repository/PriceHistoryRepository.java
+++ src/main/java/com/example/saleCampaign/Repository/PriceHistoryRepository.java
@@ -3,7 +3,7 @@ package com.example.saleCampaign.Repository;
 import com.example.saleCampaign.Model.PriceHistory;
 import com.example.saleCampaign.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query; 
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
