28298056c2528921b6d9af6ba3a4134138f17e7e
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-20 16:35:58 +0530

    Update SaleCampaignApplication.java

--- src/main/java/com/example/saleCampaign/SaleCampaignApplication.java
+++ src/main/java/com/example/saleCampaign/SaleCampaignApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class SaleCampaignApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(SaleCampaignApplication.class, args);
 	}
 
